553ea93dd712874d147deb6cf4eb6bf8471d8999
Nguyễn Quốc Thắng <nguyenqthangwork@gmail.com>
2025-04-22 14:34:23 +0700

    docs route update for prod

--- src/routes/v1/docs.route.js
+++ src/routes/v1/docs.route.js
@@ -7,19 +7,31 @@ const router = express.Router();
 
 const specs = swaggerJsdoc({
   swaggerDefinition,
-  apis: ['src/docs/*.yml', 'src/routes/v1/*.js'],
+  apis: ['./src/docs/*.yml', './src/routes/v1/*.js'],
 });
 
+// Serve Swagger JSON
+router.get('/json', (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.send(specs);
+});
+
+// Redirect /v1/docs to /v1/docs/
+router.get('', (req, res) => {
+  res.redirect('/v1/docs/');
+});
+
+// Serve Swagger UI
 router.use('/', swaggerUi.serve);
 router.get(
   '/',
   swaggerUi.setup(specs, {
     explorer: true,
     swaggerOptions: {
-      url: '/v1/docs',
-      defaultModelsExpandDepth: -1,  
-      docExpansion: 'none',  
-      tryItOutEnabled: true,  
+      url: '/v1/docs/json',
+      defaultModelsExpandDepth: -1,
+      docExpansion: 'none',
+      tryItOutEnabled: true,
       requestInterceptor: (req) => {
         console.log(`Swagger request: ${req.method} ${req.url}`);
         return req;
@@ -29,3 +41,35 @@ router.get(
 );
 
 module.exports = router;
+
+// const express = require('express');
+// const swaggerJsdoc = require('swagger-jsdoc');
+// const swaggerUi = require('swagger-ui-express');
+// const swaggerDefinition = require('../../docs/swaggerDef');
+
+// const router = express.Router();
+
+// const specs = swaggerJsdoc({
+//   swaggerDefinition,
+//   apis: ['src/docs/*.yml', 'src/routes/v1/*.js'],
+// });
+
+// router.use('/', swaggerUi.serve);
+// router.get(
+//   '/',
+//   swaggerUi.setup(specs, {
+//     explorer: true,
+//     swaggerOptions: {
+//       url: '/v1/docs',
+//       defaultModelsExpandDepth: -1,  
+//       docExpansion: 'none',  
+//       tryItOutEnabled: true,  
+//       requestInterceptor: (req) => {
+//         console.log(`Swagger request: ${req.method} ${req.url}`);
+//         return req;
+//       },
+//     },
+//   })
+// );
+
+// module.exports = router;
