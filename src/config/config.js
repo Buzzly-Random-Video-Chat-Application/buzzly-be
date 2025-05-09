290b90af497240edf969124c6155e109ae500ca7
Nguyen Quoc Thang <nguyenqthangwork@gmail.com>
2025-05-09 16:18:12 +0700

    Config Refactor

--- src/config/config.js
+++ src/config/config.js
@@ -2,7 +2,7 @@ const dotenv = require('dotenv');
 const path = require('path');
 const Joi = require('joi');
 
-dotenv.config({ path: path.join(__dirname, '../../.env.development') });
+dotenv.config({ path: path.join(__dirname, '../../.env.production') });
 
 const envVarsSchema = Joi.object()
   .keys({
